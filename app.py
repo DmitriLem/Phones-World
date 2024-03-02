72a97a284031e68f11653bb771bd1bfd89bf05c3
Dmitri Lemberski <dima.csharp.07@gmail.com>
2024-03-02 18:29:09 -0500

    Updated

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-#Flask Application
-
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 import pyodbc
+import requests
+import random
 
 app = Flask(__name__)
 
@@ -18,22 +18,48 @@ def create_connection():
                           ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
     return conn
 
+# Функция для определения местоположения пользователя по IP
+def get_location_from_ip(ip_address, api_token):
+    url = f'https://ipinfo.io/?token={api_token}&ip={ip_address}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        city = data.get('city')
+        postal_code = data.get('postal')
+        return city, postal_code
+    else:
+        return ''
+
 @app.route('/')
 def index():
+    user_ip = request.remote_addr
+
+    city, postal_code = get_location_from_ip(user_ip, '86c960f33f9c64') #Api token
 
     # Создаем соединение с базой данных
     conn = create_connection()
     cursor = conn.cursor()
 
-    # Пример выполнения запроса к базе данных
     cursor.execute("SELECT * FROM Categories")
     categories = cursor.fetchall()
+
+    # Получаем данные из таблицы Products
+    cursor.execute("SELECT * FROM Products")
+    products = cursor.fetchall()
+
+    # Получаем данные из таблицы Inventory
+    cursor.execute("SELECT * FROM Inventory")
+    inventory = cursor.fetchall()
 
     # Закрываем соединение
     conn.close()
 
-    return render_template("index.html", categories=categories)
+    filtered_categories = [category for category in categories if len(category.name.split()) <= 2][:17]
+
+    # Выбираем случайные 8 продуктов
+    random_products = random.sample(products, min(8, len(products)))
+
+    return render_template("index.html", categories=filtered_categories, city=city, postal_code=postal_code, random_products=random_products, inventory=inventory)
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
